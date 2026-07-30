ae039936aa8e3110333e30d826083062bd673ee0
RedFlameKen <nettolacaba@gmail.com>
2026-07-30 12:08:12 +0800

    feat: more content

--- Form1.cs
+++ Form1.cs
@@ -52,6 +52,7 @@ public partial class Form1 : Form
             icon = "laptop_color.svg",
             items = new List<SettingComponent>(){
                 new ButtonListDetails(){
+                    id="system",
                     buttons = new List<ButtonDetails>(){
                         new ButtonDetails(){
                             id = "display",
@@ -156,24 +157,215 @@ public partial class Form1 : Form
             id = "network_and_internet",
             icon = "wifi_color.svg",
             items = new List<SettingComponent>(){
+                new ButtonListDetails(){
+                    id="network",
+                    buttons = new List<ButtonDetails>(){
+                        new ButtonDetails(){
+                            id = "wifi",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "ethernet",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "vpn",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "mobile_hotspot",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "airplane_mode",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "proxy",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "dial-up",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "advanced_network_settings",
+                            icon = "storage.svg",
+                        },
+                    }
+                }
             }
         },
         new NavMenuDetails() {
             id = "personalization",
             icon = "brush_color.svg",
             items = new List<SettingComponent>(){
+                new ButtonListDetails(){
+                    id="personalization",
+                    buttons = new List<ButtonDetails>(){
+                        new ButtonDetails(){
+                            id = "background",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "colors",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "themes",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "dynamic_lighting",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "lock_screen",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "text_input",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "start",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "taskbar",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "fonts",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "device_usage",
+                            icon = "storage.svg",
+                        },
+                    }
+                }
             }
         },
         new NavMenuDetails() {
             id = "apps",
             icon = "apps_color.svg",
             items = new List<SettingComponent>(){
+                new ButtonListDetails(){
+                    id="apps",
+                    buttons = new List<ButtonDetails>(){
+                        new ButtonDetails(){
+                            id = "installed_apps",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "advanced_app_settings",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "default_apps",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "actions",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "offline_maps",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "apps_for_websites",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "video_playback",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "startup",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "resume",
+                            icon = "storage.svg",
+                        },
+                    }
+                }
             }
         },
         new NavMenuDetails() {
             id = "accounts",
             icon = "person_color.svg",
             items = new List<SettingComponent>(){
+                new ButtonListDetails(){
+                    id="account_settings",
+                    hasTitle = true,
+                    buttons = new List<ButtonDetails>(){
+                        new ButtonDetails(){
+                            id = "your_info",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "sign-in_options",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "linked_devices",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "your_accounts",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "family",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "windows_backup",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "other_users",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "access_work_or_school",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "passkeys",
+                            icon = "storage.svg",
+                        },
+                    }
+                },
+                new ButtonListDetails(){
+                    id="related_settings",
+                    hasTitle = true,
+                    buttons = new List<ButtonDetails>(){
+                        new ButtonDetails(){
+                            id = "account_privacy",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "subscriptions",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "payment_options",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "order_history",
+                            icon = "storage.svg",
+                        },
+                        new ButtonDetails(){
+                            id = "account_and_billing_help",
+                            icon = "storage.svg",
+                        },
+                    }
+                }
             }
         },
         new NavMenuDetails() {
